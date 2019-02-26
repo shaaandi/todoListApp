19bcd3c3f566c04ac7f27e6b8d2a8fec83129ade
Shaaandi <deviloversaad@gmail.com>
2019-02-26 21:33:53 +0500

    Updates

---
+++ precache-manifest.a36c714fef541d258f7b8d5d51523425.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3298c0cb37b5f502cde",
+    "url": "/todoListApp/static/js/runtime~main.a3298c0c.js"
+  },
+  {
+    "revision": "6d9c9a8a4020725df048",
+    "url": "/todoListApp/static/js/main.6d9c9a8a.chunk.js"
+  },
+  {
+    "revision": "7c25e639cc94b646a97b",
+    "url": "/todoListApp/static/js/2.7c25e639.chunk.js"
+  },
+  {
+    "revision": "6d9c9a8a4020725df048",
+    "url": "/todoListApp/static/css/main.e470b1e8.chunk.css"
+  },
+  {
+    "revision": "ec6f5582b778280dd0e3bf8814ad5729",
+    "url": "/todoListApp/index.html"
+  }
+];
