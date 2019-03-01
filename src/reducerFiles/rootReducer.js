2e9d55a92eaaf3aa402f9593811c6014189c101a
Shaaandi <deviloversaad@gmail.com>
2019-03-01 11:18:50 +0500

    Clearing the initial stage

--- src/reducerFiles/rootReducer.js
+++ src/reducerFiles/rootReducer.js
@@ -1,47 +1,7 @@
 import { ADD_TODO,PREV_TODO, ADD_SUB_TODO, ADD_SUB_PREV_TODO, TOGGELE_TODO_STATE, TOGGELE_PREV_TODO_STATE, TOGGELE_SUB_TODO_STATE, DELETE_TODO } from "../actionFiles/actionConstants";
 
 const initialTodoState = {
-    todos : [{
-        title: 'Udemy',
-        flag: 'orange',
-        key: 'c097bbbbafb6d7459b3d7e5ffd844abb16f47d19c1b5ac620ac1261d29a9c649',
-        state: false,
-        steps: [
-          {
-            title: 'Electricity',
-            state: false,
-            key: 'a8b41baa4cf766fa8af5a5c4236eeb5f2eba16720b139711909bb29ac3f83385',
-            parentKey: 'c097bbbbafb6d7459b3d7e5ffd844abb16f47d19c1b5ac620ac1261d29a9c649'
-          },
-          {
-            title: 'Waves',
-            state: false,
-            key: 'ad5b8321811a1ca3113a13e1436e88855625d0117518804888c682146d0638c5',
-            parentKey: 'c097bbbbafb6d7459b3d7e5ffd844abb16f47d19c1b5ac620ac1261d29a9c649'
-          },
-          {
-            title: 'Particle Physics',
-            state: false,
-            key: '86071faa978e64452f0fb6d808dfe3e3a9a8f78f61115c9a3c896038596548c4',
-            parentKey: 'c097bbbbafb6d7459b3d7e5ffd844abb16f47d19c1b5ac620ac1261d29a9c649'
-          }
-        ]
-      },
-      {
-        title: 'Computer Science',
-        flag: 'green',
-        key: '445ddaa59df684ea81e04e1b2aad2ec113a4a8a36ce8c5e802c1542266b8921d',
-        state: false,
-        steps: []
-      },
-      {
-        title: 'Physics',
-        flag: 'blue',
-        key: '349096910d3c474b61ec0cc741486084dbd9c8f2a57e37557b35c7ab5e6205f3',
-        state: false,
-        steps: []
-      }
-]
+    todos : []
 }
 
 
